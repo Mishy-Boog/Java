84c1e338797bc6615653936e5bdc652018bc61b7
Mishy-Boog <mah.hil@outlook.com>
2025-03-18 10:21:28 -0300

    Create cliente.java

---
+++ aula2/cliente.java
@@ -0,0 +1,25 @@
+public class cliente {
+    private String Nome;
+    private String CPF;
+
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getCPF() {
+        return this.CPF;
+    }
+
+    public void setNome(){
+        this.Nome = Nome;
+    }
+
+    public String getNome(){
+        return this.Nome;
+    }
+
+}
+
+
+
